4da5a2297911e3de5872a444f4500c0041f26053
treamy24 <travisreamy15@gmail.com>
2024-07-18 11:27:36 -0600

    added more to budget tools html

--- src/app/forms.py
+++ src/app/forms.py
@@ -9,6 +9,7 @@ class SignUpForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
 class LoginForm(FlaskForm):
